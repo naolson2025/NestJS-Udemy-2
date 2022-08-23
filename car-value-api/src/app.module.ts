9d752996590e00201f65a2fb1033f45b6eaff2c7
naolson2025 <45021396+naolson2025@users.noreply.github.com>
2022-08-23 07:47:04 -0500

    added DB

--- car-value-api/src/app.module.ts
+++ car-value-api/src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      // this will automatically change the structure of DB tables (add & edit tables)
+      // this is only used in development because it could cause data loss
+      // in prod we use a DB migration process to change tables (ex: add column)
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- car-value-api/src/main.ts
+++ car-value-api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // validates that any extra properties not part of the dto
+      // are stripped away
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
