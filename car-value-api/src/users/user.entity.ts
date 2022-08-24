319812df21e4ee7763cc46a9a078cdf97196d7f9
naolson2025 <45021396+naolson2025@users.noreply.github.com>
2022-08-24 06:23:52 -0500

    added logic to user service

--- car-value-api/src/users/user.entity.ts
+++ car-value-api/src/users/user.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -10,4 +17,23 @@ export class User {
 
   @Column()
   password: string;
+
+  // Here we have 3 examples of adding logic to an entity file
+  // We can run a function AfterInsert that will log the id of the inserted user
+  // it is important to create() an instance of an entity before save() because
+  // otherwise we will lose this functionality
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with this id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('removed user with id', this.id);
+  }
 }
--- car-value-api/src/users/users.service.ts
+++ car-value-api/src/users/users.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // @InjectRepository(User) this tells the dependency injection system that we
+  // need the User repository. This decorator is required because User is a generic type
+  // Repository<User> type annotation for typeorm User
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    // The create function makes an instance of a User entity
+    const user = this.repo.create({ email, password });
+    // the save() function adds the user instance to the DB
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    // this will return one value or null
+    // findOne() is a built in function on the TypeORM repository api
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    // will return an array of all matching search criteria
+    // if there are no results we will get back an empty array
+    // this is a built in function on the TypeORM repository api
+    return this.repo.find({ where: { email } });
+  }
+
+  // Partial is a Typescript annotation that says it can have some of the User properties
+  // here we fetch the user we want then save the changes
+  // this is somewhat inefficient because it makes 2 calls to the DB.
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  remove() {}
+}
